7265fed1cb3b72fa7e997339a54021fc331168a0
leoroca98 <leoroca12345@gmail.com>
2023-06-29 15:32:19 +0200

    inizio esercizio

---
+++ css/js/main.js
@@ -0,0 +1,32 @@
+let name = prompt("inserisci il tuo nome");
+
+let surname = prompt("inserisci il tuo cognome");
+
+let preferiteColor = prompt("inserisci il tuo colore preferito");
+
+
+let mioTesto = ("nomecognomecolorepreferito21");
+console.log(mioTesto);
+
+
+let bigliettoDaVisita = '--------------Biglietto da visita----------------'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
